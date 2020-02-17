d90edfeb1cddba4652ec5ae2f9889dc84e0fa794
Danny Hurtado <dannylhurtado@gmail.com>
2020-02-16 23:45:03 -0500

    allow hosts

--- negocios_push/negocios_push/settings.py
+++ negocios_push/negocios_push/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=aoce(+fe95^52f-24rvq1d&0-bvlhs3qt00d0+(5=fjt+))zi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
