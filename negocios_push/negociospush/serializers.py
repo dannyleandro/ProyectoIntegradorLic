c194503a912623ca8ab435720ac7e082706c097b
Danny Hurtado <dannylhurtado@gmail.com>
2020-03-08 23:56:41 -0500

    some files missing

---
+++ negocios_push/negociospush/serializers.py
@@ -0,0 +1,56 @@
+from rest_framework import serializers
+from .models import Profile, Process, Product
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'IdProfile',
+            'Description',
+            'City',
+            'State',
+            'ProductCode',
+            'User',
+        )
+        model = Profile
+
+
+class ProductSerializer (serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'SegmentCode',
+            'SegmentName',
+            'FamilyCode',
+            'FamilyName',
+            'ClassCode',
+            'ClassName',
+            'ProductCode',
+            'ProductName'
+        )
+        model = Product
+
+
+class ProcessSerializer (serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'IdProcess',
+            'EntityCode ',
+            'EntityName ',
+            'EntityNIT',
+            'ProcessType',
+            'ProcessNumber',
+            'ProcessState',
+            'ExecutionCity',
+            'IdProcessType',
+            'ProcessTypeName',
+            'SegmentCode',
+            'FamilyCode',
+            'ClassCode',
+            'Description',
+            'ContractType',
+            'LoadDate',
+            'SystemLoadDate',
+            'Amount',
+            'DefinitiveAmount'
+        )
+        model = Process
