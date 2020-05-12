22771bf55988f661677aa9e897bd76a6019167e7
Danny Hurtado <dannylhurtado@gmail.com>
2020-05-11 23:32:51 -0500

    Some code fixes

--- Worker/notifier.py
+++ Worker/notifier.py
@@ -1,6 +1,6 @@
-import sys, os
+import os  # sys
 import psycopg2
-from apscheduler.schedulers.blocking import BlockingScheduler
+# from apscheduler.schedulers.blocking import BlockingScheduler
 from psycopg2.extras import RealDictCursor
 from datetime import datetime
 import boto3
@@ -8,42 +8,44 @@ from botocore.exceptions import ClientError
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 
-def crearNotificaciones():
+def crear_notificaciones():
     print('Inicia')
     try:
-        conn = psycopg2.connect(host=os.environ['HOSTDB'], port=os.environ['PORTDB'], database=os.environ['NAMEDB'],
-                                user=os.environ['USERDB'], password=os.environ['PASSDB'])
-        cur = conn.cursor(cursor_factory=RealDictCursor)
-        queryCodes = """
+        connection = psycopg2.connect(host=os.environ['HOSTDB'], port=os.environ['PORTDB'],
+                                      database=os.environ['NAMEDB'], user=os.environ['USERDB'],
+                                      password=os.environ['PASSDB'])
+        cur = connection.cursor(cursor_factory=RealDictCursor)
+        query_codes = """
                     SELECT "IdProcess", "ClassCode" FROM public.negociospush_process
                     WHERE "SystemLoadDate" > now() - INTERVAL '1 day' limit 10
                     """
         print('select negociospush_process')
-        cur.execute(queryCodes)
+        cur.execute(query_codes)
         query_results = cur.fetchall()
         print(cur.rowcount)
         for row in query_results:
-            IdProcess = row['IdProcess']
-            lowerProductCode = int(row['ClassCode']) * 100
-            upperProductCode = (int(row['ClassCode']) + 1) * 100
+            id_process = row['IdProcess']
+            lower_product_code = int(row['ClassCode']) * 100
+            upper_product_code = (int(row['ClassCode']) + 1) * 100
             query = """
                     SELECT "ProductCode_id", "User_id" FROM public.negociospush_usercode 
                     WHERE "ProductCode_id">=%s and "ProductCode_id"<%s;
                     """
             print('select negociospush_usercode')
-            cur.execute(query, (lowerProductCode, upperProductCode))
+            cur.execute(query, (lower_product_code, upper_product_code))
             codes = cur.fetchall()
             # print(codes)
             print(cur.rowcount)
             for code in codes:
                 user_id = int(code['User_id'])
                 # Se crean las notificaciones en BD
-                queryNotifications = """
+                query_notifications = """
                                     SELECT id, message, read, sent, "SystemLoadDate", recipient_id
-                                    FROM public.negociospush_notification WHERE NOT sent AND recipient_id = %s ORDER BY id DESC;
+                                    FROM public.negociospush_notification WHERE NOT sent AND recipient_id = %s ORDER BY 
+                                    id DESC;
                                     """
                 print('select negociospush_notification')
-                cur.execute(queryNotifications, (user_id,))
+                cur.execute(query_notifications, (user_id,))
                 if cur.fetchone() is None:
                     insert_notifications_query = """
                                                 INSERT INTO public.negociospush_notification(
@@ -53,10 +55,13 @@ def crearNotificaciones():
                     dt = datetime.now()
                     print('Insert negociospush_notification')
                     cur.execute(insert_notifications_query, (
-                    "Tienes procesos relacionados a tu actividad económica que fueron convocados recientemente", False,
-                    False, dt, user_id))
+                        "Tienes procesos relacionados a tu actividad económica que fueron convocados recientemente",
+                        False,
+                        False, dt, user_id))
                 notifs = cur.fetchall()
                 print(cur.rowcount)
+                dt = datetime.now()
+                notification_id = ""
                 for notif in notifs:
                     notification_id = notif['id']
                 insert_notifications_detail_query = """
@@ -65,8 +70,8 @@ def crearNotificaciones():
                                                     VALUES (%s, %s, %s);
                                                     """
                 print('Insert negociospush_notificationprocesses')
-                cur.execute(insert_notifications_detail_query, (dt, notification_id, IdProcess))
-        conn.commit()
+                cur.execute(insert_notifications_detail_query, (dt, notification_id, id_process))
+        connection.commit()
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
         # exc_type, exc_obj, exc_tb = sys.exc_info()
@@ -74,87 +79,89 @@ def crearNotificaciones():
         # print(exc_type, fname, exc_tb.tb_lineno)
         raise error
     finally:
-        if conn is not None:
-            conn.close()
+        if connection is not None:
+            connection.close()
 
 
-def enviarCorreos():
+def enviar_correos():
     print('Inicia')
     try:
-        conn = psycopg2.connect(host=os.environ['HOSTDB'], port=os.environ['PORTDB'], database=os.environ['NAMEDB'],
-                                user=os.environ['USERDB'], password=os.environ['PASSDB'])
-        cur = conn.cursor(cursor_factory=RealDictCursor)
-        queryNotifications = """
-                    SELECT n.id, n.message, u.first_name, u.last_name, u.email --message, read, sent, "SystemLoadDate", recipient_id, 
+        connection = psycopg2.connect(host=os.environ['HOSTDB'], port=os.environ['PORTDB'],
+                                      database=os.environ['NAMEDB'],
+                                      user=os.environ['USERDB'], password=os.environ['PASSDB'])
+        cur = connection.cursor(cursor_factory=RealDictCursor)
+        query_notifications = """
+                    SELECT n.id, n.message, u.first_name, u.last_name, u.email
                     FROM public.negociospush_notification n 
                     INNER JOIN public.auth_user u on n.recipient_id = u.id 
                     WHERE NOT sent;
                     """
         print('select negociospush_notification')
-        cur.execute(queryNotifications)
+        cur.execute(query_notifications)
         notifications = cur.fetchall()
         for notif in notifications:
             recip = notif['email']
             mess = notif['message']
             name = notif['first_name'] + ' ' + notif['first_name']
             notification_id = notif['id']
-            queryNotificationsProcesses = """
-                    SELECT p."EntityName", p."ProcessNumber", p."ExecutionCity", p."Description", p."Amount" FROM public.negociospush_notificationprocesses n
+            query_notifications_processes = """
+                    SELECT p."EntityName", p."ProcessNumber", p."ExecutionCity", p."Description", p."Amount" 
+                    FROM public.negociospush_notificationprocesses n
                     INNER JOIN public.negociospush_process p on n.process_id = p."IdProcess"
                     WHERE n.parent_id = %s limit 3
                     """
-            cur.execute(queryNotificationsProcesses, (notification_id,))
-            notifProc = cur.fetchall()
-            textProcesses = ""
-            htmlProcesses = "<br/><br/><ul>"
-            for notproc in notifProc:
+            cur.execute(query_notifications_processes, (notification_id,))
+            notif_proc = cur.fetchall()
+            text_processes = ""
+            html_processes = "<br/><br/><ul>"
+            for notproc in notif_proc:
                 entity = notproc['EntityName']
-                processNumber = notproc['ProcessNumber']
+                process_number = notproc['ProcessNumber']
                 cities = notproc['ExecutionCity']
                 description = notproc['Description']
                 amount = str(notproc['Amount'])
-                htmlProcesses += """
+                html_processes += f"""
                                 <br/>
-                                <li>Número del proceso """ + processNumber + """</li>
-                                <li>Convocado por la entidad """ + entity + """</li>
-                                <li>A ser ejecutado en """ + cities + """</li>
-                                <li>""" + description + """</li>
-                                <li>Cuantía """ + amount + """</li>
+                                <li>Número del proceso {process_number}</li>
+                                <li>Convocado por la entidad {entity}</li>
+                                <li>A ser ejecutado en {cities}</li>
+                                <li>{description}</li>
+                                <li>Cuantía {amount}</li>
                                 <br/>
                                 """
-                textProcesses += """
-                                -Número del proceso """ + processNumber + """ \r\n
-                                Convocado por la entidad """ + entity + """ \r\n
-								 A ser ejecutado en """ + cities + """ \r\n
-								 """ + description + """ \r\n
-								 Cuantía """ + amount + """ \r\n
-								 \r\n
-								 \r\n
-								 """
-            htmlProcesses += "</ul><br/><br/>"
-            BODY_TEXT = (""" Estimad@ """ + name + """ \r\n
-	             		""" + mess + """ \r\n
-	             		""" + textProcesses + """\r\n
-	             		Ingresa a nuestra pagina para más información. \r\n \r\n
-		 	            Este correo es generado automaticamente con Amazon SES
-	            		"""
+                text_processes += f"""
+                                -Número del proceso {process_number} \r\n
+                                Convocado por la entidad {entity} \r\n
+                                A ser ejecutado en {cities} \r\n
+                                {description} \r\n
+                                Cuantía {amount} \r\n
+                                \r\n
+                                \r\n
+                                """
+            html_processes += "</ul><br/><br/>"
+            body_text = (f""" Estimad@ {name} \r\n
+                        {mess} \r\n
+                        {text_processes} \r\n
+                        Ingresa a nuestra pagina para más información. \r\n \r\n
+                        Este correo es generado automaticamente con Amazon SES
+                        """
                          )
 
             # The HTML body of the email.
-            BODY_HTML = """<html>
-							<head></head>
-							<body>
-							  <h2>Estimad@ """ + name + """</h2>
-							  <h3>""" + mess + """</h3>
-							  """ + htmlProcesses + """
-							  <h3>Ingresa a nuestra pagina para más información</h3>
-						  	  <p>Este correo es generado automaticamente
-						      <a href='https://aws.amazon.com/ses/'>Amazon SES</a></p>
-							</body>
-						</html>
-						"""
-            sendEmail(recip, BODY_TEXT, BODY_HTML)
-        conn.commit()
+            body_html = f"""<html>
+                            <head></head>
+                            <body>
+                                <h2>Estimad@ {name}</h2>
+                                <h3>{mess}</h3>
+                                {html_processes}
+                                <h3>Ingresa a nuestra pagina para más información</h3>
+                                <p>Este correo es generado automaticamente
+                                <a href='https://aws.amazon.com/ses/'>Amazon SES</a></p>
+                            </body>
+                            </html>
+                        """
+            send_email(recip, body_text, body_html)
+        connection.commit()
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
         # exc_type, exc_obj, exc_tb = sys.exc_info()
@@ -162,30 +169,28 @@ def enviarCorreos():
         #  print(exc_type, fname, exc_tb.tb_lineno)
         raise error
     finally:
-        if conn is not None:
-            conn.close()
+        if connection is not None:
+            connection.close()
 
 
-def sendEmail(RECIPIENT, BODY_TEXT, BODY_HTML):
+def send_email(recipient, body_text, body_html):
     print('Se envía correo a utilizando SES')
-    # Let's use Amazon S3
-    s3 = boto3.resource('s3')
     # Replace sender@example.com with your "From" address.
     # This address must be verified with Amazon SES.
-    SENDER = "Negocios Push <dannylhurtado@gmail.com>"
+    sender = "Negocios Push <dannylhurtado@gmail.com>"
 
     # If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
-    AWS_REGION = "us-east-1"
+    aws_region = "us-east-1"
 
     # The subject line for the email.
-    SUBJECT = "Nuevos procesos convocados"
+    subject = "Nuevos procesos convocados"
 
     # The character encoding for the email.
-    CHARSET = "UTF-8"
+    charset = "UTF-8"
 
     # Create a new SES resource and specify a region.
     client = boto3.client('ses',
-                          region_name=AWS_REGION,
+                          region_name=aws_region,
                           aws_access_key_id=os.environ['SESKEYID'],
                           aws_secret_access_key=os.environ['SESSECRETKEY'])
     # Try to send the email.
@@ -194,26 +199,26 @@ def sendEmail(RECIPIENT, BODY_TEXT, BODY_HTML):
         response = client.send_email(
             Destination={
                 'ToAddresses': [
-                    RECIPIENT,
+                    recipient,
                 ],
             },
             Message={
                 'Body': {
                     'Html': {
-                        'Charset': CHARSET,
-                        'Data': BODY_HTML,
+                        'Charset': charset,
+                        'Data': body_html,
                     },
                     'Text': {
-                        'Charset': CHARSET,
-                        'Data': BODY_TEXT,
+                        'Charset': charset,
+                        'Data': body_text,
                     },
                 },
                 'Subject': {
-                    'Charset': CHARSET,
-                    'Data': SUBJECT,
+                    'Charset': charset,
+                    'Data': subject,
                 },
             },
-            Source=SENDER,
+            Source=sender,
             # If you are not using a configuration set, comment or delete the
             # following line
         )
@@ -232,8 +237,8 @@ sched = BlockingScheduler()
 @sched.scheduled_job('cron', hour=1, minute=30)
 def scheduled_job():
     print('Tarea programada diariamente a la 1:30am')
-    crearNotificaciones()
-    enviarCorreos()
+    crear_notificaciones()
+    enviar_correos()
 
 
 sched.start()
